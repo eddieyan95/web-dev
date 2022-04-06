8678240bc2430a6e068590b2515427e0ad08686f
eddieyan95 <yufanyan95@gmail.com>
2022-04-06 19:14:01 -0400

    a8 REST sp22

--- src/components/Tuiter/reducers/tuits-reducer.js
+++ src/components/Tuiter/reducers/tuits-reducer.js
@@ -1,50 +1,27 @@
-import tuits from "../data/tuits.json";
+// import tuits from "../data/tuits.json";
+import {DELETE_TUIT, FIND_ALL_TUITS,UPDATE_TUIT,CREATE_TUIT}
+    from "../../../actions/tuits-actions"
 
-const tuitsReducer =
-    (state = tuits, action) => {
-        switch (action.type) {
-            case 'like-tuit':
-                return state.map(tuit => {
-                    if(tuit._id === action.tuit._id) {
-                        if(tuit.liked === true) {
-                            tuit.liked = false;
-                            tuit.stats.likes--;
-                        } else {
-                            tuit.liked = true;
-                            tuit.stats.likes++;
-                        }
-                        return tuit;
-                    } else {
-                        return tuit;
-                    }
-                });
-
-
-            case 'delete-tuit':
-                return state.filter(
-                    tuit => tuit._id !== action.tuit._id);
-
-            case 'create-tuit':
-                const newTuit = {
-                    tuit: action.tuit,
-                    _id: (new Date()).getTime() + '',
-                    postedBy: {
-                        "username": "ReactJS"
-                    },
-                    stats: {
-                        retuits: 111,
-                        likes: 222,
-                        replies: 333
-                    }
-                }
-                return [
-                    newTuit,
-                    ...state,
-                ];
-            default:
-                return tuits
-        }
-    };
+const tuitsReducer = (state = [], action) => {
+    switch (action.type) {
+        case UPDATE_TUIT:
+            return state.map(
+                tuit => tuit._id === action.tuit._id ?
+                        action.tuit : tuit);
+        case CREATE_TUIT:
+            return [
+                action.newTuit,
+                ...state
+            ];
+        case FIND_ALL_TUITS:
+            return action.tuits;
+        case DELETE_TUIT:
+            return state.filter(
+                tuit => tuit._id !== action.tuit._id);
+        default:
+            return state;
+    }
+}
 
 
 export default tuitsReducer;
--- src/components/Tuiter/tuit-list/index.js
+++ src/components/Tuiter/tuit-list/index.js
@@ -1,25 +1,78 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import {useDispatch, useSelector}
+    from "react-redux";
+import * as service
+    from '../../../services/tuits-service';
+
 // import tuits from "./tuits.json"; //import the tuits
 import TuitListItem from "./tuit-list-item";
 // import './tuits.css';
-import {useSelector} from "react-redux";
-
+// import {useSelector} from "react-redux";
+import {deleteTuit,findAllTuits,createTuit,updateTuit}
+    from "../../../actions/tuits-actions";
 
 const TuitList = () => {
-    const tuits = useSelector(state => state.tuits);
-
-
+    const tuits = useSelector(
+        state => state.tuits);
+    const [newTuit, setNewTuit] = useState({tuit: 'New tuit'});
+    const dispatch = useDispatch();
+    useEffect(() =>
+                  findAllTuits(dispatch),
+              [dispatch]);
     return (
-
-        <ul className="ttr-tuits list-group">
+        <ul className="list-group">
+            <li className="list-group-item">
+                <button onClick={() => createTuit(dispatch, newTuit)}
+                        className="btn btn-primary float-end">Tuit</button>
+                <textarea className="form-control w-75" onChange={(e) =>
+                    setNewTuit({...newTuit, tuit: e.target.value})}></textarea>
+            </li>
             {
-
-                tuits.map && tuits.map(tuit => //iterate over each tuit and render it as a tuitlistitem
-                                            <TuitListItem key={tuit._id}
-                                                         tuit={tuit}/>)
+                tuits.map(tuit =>
+                              <li key={tuit._id}
+                                  className="list-group-item">
+                                  <i className="fas fa-remove float-end"
+                                     onClick={() => deleteTuit(dispatch, tuit)}></i>
+                                  {tuit.tuit}
+                                  <div>
+                                      Likes: {tuit.likes}
+                                      <i onClick={() => updateTuit(dispatch, {
+                                          ...tuit, likes: tuit.likes + 1
+                                      })} className="far fa-thumbs-up ms-2"></i>
+                                  </div>
+                              </li>)
             }
         </ul>
     );
 }
+// const TuitList = () => {
+//     const tuits = useSelector(state => state.tuits);
+//     const dispatch = useDispatch();
+//     // const findAllTuits = async () => {
+//     //     const tuits = await service.findAllTuits();
+//     //     dispatch({
+//     //                  type: 'FIND_ALL_TUITS',
+//     //                  tuits: tuits
+//     //              });
+//     // }
+//     // useEffect(findAllTuits, []);
+//     useEffect(() =>
+//                   findAllTuits(dispatch),
+//               []);
+//
+//
+//
+//     return (
+//
+//         <ul className="ttr-tuits list-group">
+//             {
+//
+//                 tuits.map && tuits.map(tuit => //iterate over each tuit and render it as a tuitlistitem
+//                                             <TuitListItem key={tuit._id}
+//                                                          tuit={tuit}/>)
+//             }
+//         </ul>
+//     );
+// }
 export default TuitList;
 
